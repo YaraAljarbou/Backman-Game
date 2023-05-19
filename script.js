1f38d722a69efa01f1ddfe37ed84d89b5fc9e723
YaraAljarbou <yara.aljarbou@yahoo.com>
2023-05-19 17:11:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,183 @@
+var layouts = [
+    {
+        id: 0,
+        layout: [
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,0,2,2,2,2,2,3,2,2,2,2,2,2,2,2,2,3,2,2,2,2,2,2,2,2,1],
+            [1,3,1,2,2,2,1,2,2,2,1,2,2,2,1,1,1,2,2,2,2,3,1,2,4,2,1],
+            [1,2,2,1,2,1,2,2,2,1,4,1,2,2,1,2,3,1,2,2,2,1,2,1,2,2,1],
+            [1,2,2,3,1,4,2,2,1,2,2,2,1,2,1,2,2,1,2,2,1,2,2,2,1,2,1],
+            [1,3,2,2,1,2,2,2,1,1,3,1,1,2,1,2,1,2,2,2,1,1,2,1,1,2,1],
+            [1,2,2,2,1,3,2,2,1,2,2,2,1,2,1,2,3,1,2,2,1,2,2,2,1,2,1],
+            [1,2,2,2,1,2,2,2,1,2,2,2,1,2,1,2,2,4,1,2,1,2,2,2,1,2,1],
+            [1,4,2,2,2,2,2,2,2,2,2,2,2,2,2,3,2,2,2,2,2,2,2,2,2,3,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        ],
+        coins: 135
+    },
+        
+    {
+        id: 1,
+        layout: [
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,0,2,2,2,2,2,3,2,2,2,2,2,2,2,2,2,3,2,2,2,2,2,2,2,2,1],
+            [1,3,1,2,2,2,1,2,2,2,1,2,2,2,1,1,1,2,2,2,2,3,1,2,4,2,1],
+            [1,2,2,1,2,1,2,2,2,1,4,1,2,2,1,2,3,1,2,2,2,1,2,1,2,2,1],
+            [1,2,2,3,1,4,2,2,1,2,2,2,1,2,1,2,2,1,2,2,1,2,2,2,1,2,1],
+            [1,3,2,2,1,2,2,2,1,1,3,1,1,2,1,2,1,2,2,2,1,1,2,1,1,2,1],
+            [1,2,2,2,1,3,2,2,1,2,2,2,1,2,1,2,3,1,2,2,1,2,2,2,1,2,1],
+            [1,2,2,2,1,2,2,2,1,2,2,2,1,2,1,2,2,4,1,2,1,2,2,2,1,2,1],
+            [1,4,2,2,2,2,2,2,2,2,2,2,2,2,2,3,2,2,2,2,2,2,2,2,2,3,1],
+            [1,2,2,1,1,1,1,2,2,2,1,2,2,2,1,2,2,2,1,2,1,1,1,1,2,2,1],
+            [1,2,1,2,2,2,2,2,2,1,2,1,2,2,1,2,2,2,1,3,1,0,0,0,1,2,1],
+            [1,2,2,1,1,1,2,4,1,2,3,2,1,2,1,2,2,2,1,4,1,0,0,0,1,2,1],
+            [1,2,2,2,2,2,1,2,1,1,2,1,1,2,1,2,2,2,1,2,1,0,0,0,1,2,1],
+            [1,2,1,3,1,1,2,2,1,2,2,2,1,2,4,1,1,1,3,2,1,1,1,1,2,2,1],
+            [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,2,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        ],
+        coins: 220
+    },
+
+    {
+        id: 2,
+        layout: [
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,0,1,2,2,2,2,3,2,2,2,1,2,2,2,2,2,3,2,2,2,2,2,2,2,2,1],
+            [1,3,1,2,2,2,1,1,1,1,2,1,2,2,2,2,2,2,2,2,2,3,2,2,4,2,1],
+            [1,2,1,2,1,1,1,2,2,1,3,1,2,1,1,1,1,1,1,1,1,1,1,1,2,2,1],
+            [1,2,1,3,2,4,1,2,2,1,2,2,2,1,2,2,1,2,2,2,2,2,2,2,2,2,1],
+            [1,3,1,1,1,1,1,2,2,1,1,1,1,1,2,2,1,2,2,1,1,1,1,1,1,1,1],
+            [1,2,1,2,1,3,1,2,2,2,2,2,2,2,2,2,3,2,2,2,2,2,1,2,2,2,1],
+            [1,2,1,2,1,2,2,2,1,1,1,1,1,1,1,1,1,4,2,1,2,2,1,2,1,2,1],
+            [1,2,1,2,1,2,2,2,1,2,2,2,2,2,2,3,1,2,2,1,2,2,1,2,1,3,1],
+            [1,2,1,2,1,2,1,1,1,1,1,2,1,1,1,2,1,2,2,1,2,2,1,2,1,2,1],
+            [1,2,1,2,1,2,2,2,2,1,2,2,2,2,2,2,2,2,2,3,2,2,1,2,1,2,1],
+            [1,2,1,2,1,2,2,4,2,1,3,2,2,1,1,1,2,2,1,4,2,2,1,2,1,2,1],
+            [1,2,1,3,2,2,2,2,2,1,2,2,2,2,1,2,2,2,1,2,2,2,1,2,1,2,1],
+            [1,2,1,1,1,1,1,1,1,1,1,1,1,2,4,1,3,1,1,1,1,1,1,2,1,2,1],
+            [1,2,2,2,2,2,2,3,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,1,2,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        ],
+        coins: 199
+    }
+];
+
+layoutNumber = Math.floor(Math.random() * 3);
+for (var layout in layouts)
+    if (layouts[layout].id == layoutNumber) {
+        var world = layouts[layout].layout;
+        var win =  layouts[layout].coins;
+    }
+        
+
+var width = world[0].length * 40;
+document.getElementById('scoreBoard').style.width = width; 
+
+var worldDict = {
+    0: 'blank',
+    1: 'wall',
+    2: 'coin',
+    3: 'cherry',
+    4: 'ghost'
+}
+
+function drawWorld() {
+    var output = '';
+
+    for (var row=0 ; row<world.length ; row++) {
+        output += '<div class="row"></div>';
+        for (var column=0 ; column<world[row].length ; column++)
+            output += '<div class="' + worldDict[world[row][column]] + '"></div>';
+    }
+    document.getElementById('world').innerHTML = output;
+}
+drawWorld();
+
+var pacman = {
+    x: 1,
+    y: 1,
+    direction: 0
+}
+
+function drawPacman() {
+    document.getElementById('pacman').style.left = pacman.x * 40 + 'px';
+    document.getElementById('pacman').style.top = pacman.y * 40 + 'px';
+    document.getElementById('pacman').style.transform = 'rotate(' + pacman.direction + 'deg)';
+}
+drawPacman();
+
+var coinCount = 0, cherryCount = 0, score = 0, lives = 3;
+
+function updateScoreBoard(score, cherryCount, coinCount, lives) {
+    document.getElementById('score').innerHTML = '<p>SCORE: ' + score + '</p>';
+
+    output = '';
+    for (var i=0 ; i<lives ; i++)
+        output += '<img src="./images/pacman.gif" alt="">';
+    document.getElementById('lives').innerHTML = output;
+
+    document.getElementById('counts').innerHTML = '<img src="./images/coin.gif" alt=""><p> ' + coinCount + '</p> <img src="./images/cherry.png" alt=""><p>' + cherryCount + '</p>';
+}
+updateScoreBoard(score, cherryCount, coinCount, lives);
+
+document.onkeydown = function(e) {
+    if (e.keyCode == 37 && world[pacman.y][pacman.x-1] != 1) { //LEFT
+        if (world[pacman.y][pacman.x-1] == 4)
+            lives--;
+        else {
+            pacman.x--;
+            pacman.direction = 180;
+        }
+    }
+    else if (e.keyCode == 39 && world[pacman.y][pacman.x+1] != 1) { // RIGHT
+        if (world[pacman.y][pacman.x+1] == 4)
+            lives--;
+        else {
+            pacman.x++;
+            pacman.direction = 0;
+        }
+    }
+    else if (e.keyCode == 40 && world[pacman.y+1][pacman.x] != 1) { // DOWN
+        if (world[pacman.y+1][pacman.x] == 4)
+            lives--;
+        else {
+            pacman.y++;
+            pacman.direction = 90;
+        }
+    }
+    else if (e.keyCode == 38 && world[pacman.y-1][pacman.x] != 1) { // UP
+        if (world[pacman.y-1][pacman.x] == 4)
+            lives--;
+        else {
+            pacman.y--;
+            pacman.direction = 270;
+        }
+    }
+
+    if (world[pacman.y][pacman.x] == 2) {
+        coinCount++;
+        win--;
+    } 
+        
+    else if (world[pacman.y][pacman.x] == 3)
+        cherryCount++;
+    
+    world[pacman.y][pacman.x] = 0;
+
+    score = (coinCount*10) + (cherryCount*50);
+    
+    drawWorld();
+    drawPacman();
+    updateScoreBoard(score, cherryCount, coinCount, lives);
+    
+    if (lives == 0) {
+        alert('Game Over *o*');
+        document.location.reload(true);
+    }
+
+    if (win == 0) {
+        alert('You Win ^.^');
+        document.location.reload(true);
+    }
+}
